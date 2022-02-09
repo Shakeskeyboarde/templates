78e810a66d633a04027bdfb71c867b7eb6c7c2a5
Chris Ackerman <chris@topher.land>
2022-02-09 11:30:14 -0800

    bin: added usage and version

--- bin/src/main.ts
+++ bin/src/main.ts
@@ -1,3 +1,16 @@
+import { usage } from './usage';
+
 export async function main(args = process.argv.slice(2)): Promise<void> {
+  if (args.includes('--help')) {
+    usage();
+    return;
+  }
+
+  if (args.includes('--version')) {
+    // eslint-disable-next-line global-require
+    console.log(require('../package.json').version);
+    return;
+  }
+
   console.log(`Hello, ${args[0] || 'World'}!`);
 }
---
+++ bin/src/usage.ts
@@ -0,0 +1,16 @@
+export function usage(): void {
+  console.log(
+    // eslint-disable-next-line prefer-template
+    `
+Usage: {{{&target}}} [options]
+       {{{&target}}} --version
+       {{{&target}}} --help
+
+{{{Description}}}
+
+Options:
+  --version  Display the current version
+  --help     Display this help text
+    `.trim() + '\n',
+  );
+}
