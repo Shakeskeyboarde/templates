2b5d2115d64e9505b1b7a64edf8502508db9733c
Chris Ackerman <chris@topher.land>
2022-02-10 13:28:28 -0800

    spa: normalized eslint and ts configs

--- spa/.eslintrc.cjs
+++ spa/.eslintrc.cjs
@@ -3,9 +3,21 @@ module.exports = {
   root: true,
   overrides: [
     {
+      files: ['*.js', '*.cjs'],
+      parserOptions: { sourceType: 'script' },
+      env: { node: true, es2021: true },
+      extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    },
+    {
       files: ['*.ts', '*.tsx'],
       parserOptions: { project: './tsconfig.json', tsconfigRootDir: __dirname },
       settings: { react: { version: 'latest' } },
+      rules: {
+        'no-console': 'warn',
+        'import/no-default-export': 'error',
+        'import/prefer-default-export': 'off',
+        'import/extensions': ['error', 'never'],
+      },
       extends: [
         'eslint:recommended',
         'airbnb',
@@ -17,16 +29,6 @@ module.exports = {
         'plugin:testing-library/react',
         'plugin:prettier/recommended',
       ],
-      rules: {
-        'import/no-default-export': 'error',
-        'import/prefer-default-export': 'off',
-      },
-    },
-    {
-      files: ['*.js', '*.cjs'],
-      parserOptions: { sourceType: 'script' },
-      env: { node: true, es2020: true },
-      extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     },
   ],
   ignorePatterns: ['node_modules', 'lib', 'out', 'dist'],
