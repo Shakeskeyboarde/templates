f9695a75a1245a77f9dd8c3a46efb8544f3c5360
Chris Ackerman <chris@topher.land>
2022-07-14 02:06:51 -0700

    webpack config improvements

--- spa/webpack.config.cjs
+++ spa/webpack.config.cjs
@@ -6,7 +6,8 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 /** @return {import('webpack').Configuration} */
 module.exports = (environment, argv) => {
   const isBuild = Boolean(environment.WEBPACK_BUILD);
-  const isProduction = argv.mode === 'production' || (!argv.mode && isBuild);
+  const modeConfig = argv.mode || process.env.NODE_ENV;
+  const mode = modeConfig || (isBuild ? 'production' : 'development');
   const outputRoot = `${__dirname}/dist`;
 
   return {
@@ -20,43 +21,58 @@ module.exports = (environment, argv) => {
       port: 8080,
       static: 'public',
     },
-    devtool: environment.WEBPACK_SERVE ? 'inline-source-map' : 'source-map',
-    entry: './src/index.tsx',
-    mode: isProduction ? 'production' : 'development',
+    devtool: mode === 'production' ? 'source-map' : 'inline-source-map',
+    entry: ['./src/index.tsx'],
+    mode,
     module: {
       rules: [
+        // Make a best attempt to downlevel and polyfill absolutely ANY
+        // script (including node_modules) to something that can be run in
+        // the browser.
         {
           exclude: [/node_modules[\\/]webpack[\\/]buildin/, /node_modules[\\/]core-js/],
-          // Make a best attempt to downlevel and polyfill absolutely ANY
-          // script (including node_modules) to something that can be run in
-          // the browser.
           test: /\.(tsx?|jsx?)$/i,
           use: 'babel-loader',
         },
+        // TypeScript > JavaScript
         {
           exclude: [/node_modules/],
-          // TypeScript > JavaScript
           test: /\.tsx?$/i,
           use: { loader: 'ts-loader', options: { configFile: 'tsconfig.build-webpack.json' } },
         },
+        // Allow CSS for backwards compatibility, but prefer CSS-in-JS for
+        // anything complex.
         {
-          // Allow CSS for backwards compatibility, but prefer CSS-in-JS for
-          // anything complex.
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+        // Prefer separate resource files to take advantage of lazy loading,
+        // parallelism, caching, and to optimize transfer size.
         {
-          // Prefer separate resource files to take advantage of lazy loading,
-          // parallelism, caching, and to optimize transfer size.
           test: /\.(gif|jpe?g|png|apng|svg|webp|bmp|ico|woff2?|otf|ttf)$/,
           type: 'asset/resource',
         },
       ],
     },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            name: (module_) =>
+              'vendor.' + module_.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1].replace('@', ''),
+            test: /[\\/]node_modules[\\/]/,
+          },
+        },
+        chunks: 'all',
+        maxInitialRequests: Number.POSITIVE_INFINITY,
+        minSize: 0,
+      },
+    },
     output: {
       assetModuleFilename: 'bundle/[name].[contenthash:8].[ext]',
       filename: 'bundle/[name].[chunkhash:8].js',
-      path: `${outputRoot}/${isProduction ? 'production' : 'development'}`,
+      path: `${outputRoot}/${mode}`,
       publicPath: 'auto',
     },
     performance: { hints: false },
@@ -68,7 +84,8 @@ module.exports = (environment, argv) => {
       }),
       new CopyPlugin({ patterns: [{ context: 'public', from: '**', noErrorOnMissing: true }] }),
       new BundleAnalyzerPlugin({
-        analyzerMode: isBuild && isProduction ? 'static' : 'disabled',
+        analyzerMode: isBuild && mode === 'production' ? 'static' : 'disabled',
+        defaultSizes: 'parsed',
         openAnalyzer: false,
         reportFilename: `${outputRoot}/report.html`,
       }),
