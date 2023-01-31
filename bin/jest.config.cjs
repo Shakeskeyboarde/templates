af6b33312d88865b4e0c2d77c261614418c9666e
Chris Ackerman <chris@topher.land>
2023-01-30 16:59:35 -0800

    removed unused dependencies

--- bin/jest.config.cjs
+++ bin/jest.config.cjs
@@ -3,22 +3,22 @@
  * https://jestjs.io/docs/configuration
  */
 
-const ignorePatterns = ['/node_modules/', '/\\.', '/_', '/index\\.tsx?$', '\\.d\\.ts$'];
+const ignorePatterns = ['/node_modules/', '/\\.', '/_', '/index\\.ts$', '\\.d\\.ts$'];
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   bail: 0,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'out/coverage',
   coveragePathIgnorePatterns: ignorePatterns,
   coverageProvider: 'v8',
   coverageReporters: ['text-summary', 'html-spa', 'lcov'],
   coverageThreshold: { global: { branches: 0, functions: 0, lines: 0, statements: 0 } },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     // Remove the .js extension (required for ES Module support) from TS file imports.
-    '^(\\.{1,2}/.*)\\.jsx?$': '$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   restoreMocks: true,
   roots: ['src'],
@@ -26,7 +26,7 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ignorePatterns,
   transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { diagnostics: { ignoreCodes: [151001] }, useESM: true }],
+    '^.+\\.[tj]s$': ['ts-jest', { diagnostics: { ignoreCodes: [151001] }, useESM: true }],
   },
   transformIgnorePatterns: ['/node_modules/jest.*'],
   verbose: true,
